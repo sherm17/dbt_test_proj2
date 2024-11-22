5995649359cf221931a11ca468a4f2578a85b19f
Sherman Peng Admin <ShermanP-Admin@cityofsanrafael.org>
2024-11-21 16:39:03 -0800

    add neighborhood to source

--- models/seeclickfix_requests_prod.sql
+++ models/seeclickfix_requests_prod.sql
@@ -9,6 +9,6 @@ SELECT
     n.name
 FROM
     {{ ref('seeclickfix_requests_stage') }} stg,
-    {{ ref('neighborhood') }} n
+    {{ source('boundary' ,'neighborhood') }} n
 WHERE ST_Contains(n.geometry, stg.geometry)
 
